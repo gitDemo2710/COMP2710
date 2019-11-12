d1dbcce19e9658d7c24ba8bf802a4d339bf6e44f
Nathan Joseph Henty <njh0018@tux052.eng.auburn.edu>
2019-11-12 11:50:07 -0600

    git_test.cpp

--- git_test.cpp
+++ git_test.cpp
@@ -12,6 +12,7 @@ int main()
 void display ()
 {
  cout << "Xuechao Li" << endl;
+cout << "Nate Henty" << endl;
  cout << "Leslie Blair Owens" << endl;
  cout << "Thomas Hansknecht" << endl;
 cout << "William Melton" << endl;
