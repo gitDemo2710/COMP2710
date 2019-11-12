0b68d2696bf827ee3ec0de1c5d26795890a9b6d4
Thomas Franklin Hansknecht <tfh0007@tux052.eng.auburn.edu>
2019-11-12 14:03:14 -0600

    I added Thomas Hansknecht to the git_test.cpp file.

--- git_test.cpp
+++ git_test.cpp
@@ -12,50 +12,6 @@ int main()
 void display ()
 {
  cout << "Xuechao Li" << endl;
- cout << "Michael Norwood" << endl;
- cout << "Craig Manes" << endl;
- cout << "David Morris" << endl;
- cout << "James Barr" << endl;
- cout << "Ryan Nagy" << endl;
- cout << "Nate Henty" << endl;
-cout << "Nate Henty" << endl;
- cout << "Jonathan Blackburn" << endl;
- cout << "Madison Austin" << endl;
- cout << "Houston Walley" << endl;
- cout << "Leslie Blair Owens" << endl;
  cout << "Thomas Hansknecht" << endl;
- cout << "William Melton" << endl;
- cout << "Caleb Fahlgren" << endl;
- cout << "Anne Marie Bonadio" << endl;
- cout << "Tyler Hirn" << endl;
- cout << "Jacob Howard" << endl;
- cout << "Elizabeth Truong" << endl;
- cout << "Jason Barbieri" << endl;
- cout << "Connor Taylor" << endl;
- cout << "Justin Tran" << endl;
- cout << "Ryan Hernandez" << endl;
- cout << "Swati Baskiyar" << endl;
- cout << "Christopher Acosta" << endl;
- cout << "Fowler Walker" <<endl;
- cout << "Haoqian Li" << endl;
- cout << "Chad Beibide" << endl;
- cout << "William Hutto" << endl;
- cout << "Devan Thomas" << endl;
- cout << "Jonathan Bartlett" << endl;
- cout << "John Spencer" << endl;
- cout << "Alfred Spies" << endl;
- cout << "Patrick Roberts" << endl;
- cout << "Rayshaun Bagsby" << endl;
- cout << "Collier Robinson" << endl;
- cout << "Tucker Adkison" << endl;
- cout << "Ty Brunner" << endl;
- cout << "Patrick Spafford" << endl;
- cout << "Dylan Dunn" << endl;
- cout << "Naman Gupta" << endl;
- cout << "Branson Holt" << endl;
- cout << "Bobby Sharp" << endl;
- cout << "Connor Morris" << endl;
- cout << "Nicolas Rios" << endl;
- cout << "Connor Morris" << endl;
 }
 
