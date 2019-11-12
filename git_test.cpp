70aa2e77bfde9073890f7b13cd450f7d66e1c152
Justin H Tran <jht0025@tux057.eng.auburn.edu>
2019-11-12 11:01:05 -0600

    Yes

--- git_test.cpp
+++ git_test.cpp
@@ -1,4 +1,4 @@
-//Quiz7
+/i/Quiz7
 
 #include <iostream>
 using namespace std;
@@ -24,15 +24,11 @@ cout << "Swati Baskiyar" << endl;
  cout << "John Spencer" << endl;
  cout << "Alfred Spies" << endl;
  cout << "Patrick Roberts" << endl;
-<<<<<<< HEAD
 cout << "Collier Robinson" << endl;
->>>>>>> e251195a0cb98a3f790535f935c26233e87e86ed
-=======
  cout << "Tucker Adkison" << endl;
  cout << "Ty Brunner" << endl;
  cout << "Patrick Spafford" << endl;
  cout << "Dylan Dunn" << endl;
->>>>>>> 529fbb72e77a54fceb7f7619f0afccd2f3bf9420
  cout << "update" << endl;
  cout << "Naman Gupta" << endl;
  cout << "Branson Holt" << endl;
