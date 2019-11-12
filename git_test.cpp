c568b694e6f5335128f243ecbbb904689c1ec145
Jonathan G Blackburn <jgb0030@tux050.eng.auburn.edu>
2019-11-12 11:50:06 -0600

    Adding name Jonathan Blackburn

--- git_test.cpp
+++ git_test.cpp
@@ -12,6 +12,7 @@ int main()
 void display ()
 {
  cout << "Xuechao Li" << endl;
+ cout << "Jonathan Blackburn" << endl;
  cout << "Madison Austin" << endl;
  cout << "Leslie Blair Owens" << endl;
  cout << "Thomas Hansknecht" << endl;
