b8535b41972153e9c897652ab34737521345df5c
John Whaley <jvw0005@tux050.eng.auburn.edu>
2019-11-12 11:42:40 -0600

    git_test.cpp

--- git_test.cpp
+++ git_test.cpp
@@ -13,5 +13,13 @@ int main()
 void display ()
 {
  cout << "Xuechao Li" << endl;
+ cout << "Leslie Blair Owens" << endl;
+ cout << "Thomas Hansknecht" << endl;
+cout << "William Melton" << endl;
+ cout << "Caleb Fahlgreen" << endl;
+ cout << "Anne Marie Bonadio" << endl;
+ cout << "Tyler Hirin" << endl;
+ cout << "Jacob Howard" << endl;
+ cout << "Byron Prather" << endl;
  cout << "John Whaley" << endl;
 }
