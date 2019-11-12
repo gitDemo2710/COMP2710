a918adf178a6da04476090eaf9bea9cf41c44c37
Patrick Roberts <prr0007@auburn.edu>
2019-11-12 10:34:56 -0600

    Fixing stuff

--- git_test.cpp
+++ git_test.cpp
@@ -13,35 +13,15 @@ void display ()
 {
  cout << "Xuechao Li" << endl;
  cout << "Chad Beibide" << endl;
-<<<<<<< HEAD
-<<<<<<< HEAD
  cout << "William Hutto" << endl;
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
  cout << "Alfred Spies" << endl;
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
  cout << "Devan Thomas" << endl;
->>>>>>> 5c6b053ab98045584b2f77863b6d7191645ef498
  cout << "Jonathan Bartlett" << endl;
  cout << "John Spencer" << endl;
  cout << "Alfred Spies" << endl;
- cout << "Fowler Walker" <<endl;
+ cout << "Fowler Walker" << endl;
  cout << "Patrick Roberts" << endl;
  cout << "Tucker Adkison" << endl;
- cout << "update" << endl;
-<<<<<<< HEAD
->>>>>>> e251195a0cb98a3f790535f935c26233e87e86ed
->>>>>>> 15c2410f4fed2ce8b5a10d5d61b0daa57695e69d
->>>>>>> 9c595be5b7d177fc0e6e4cb3276340ad14b6a379
-=======
->>>>>>> 5c6b053ab98045584b2f77863b6d7191645ef498
+ cout << "Ty Brunner" << endl;
 }
 
