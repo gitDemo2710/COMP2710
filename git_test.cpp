49c686e1e9d0abb23174f8110909788751aff91d
Panyang Chen <pzc0029@tux063.eng.auburn.edu>
2019-11-12 20:09:57 -0600

    v5

--- git_test.cpp
+++ git_test.cpp
@@ -19,8 +19,9 @@ cout << "William Melton" << endl;
  cout << "Anne Marie Bonadio" << endl;
  cout << "Tyler Hirn" << endl;
  cout << "Jacob Howard" << endl;
- cout<<"panyang chen"<<endl;
- cout << "Byron Prather" << endl;
+ cout<<"panyang chen"<<endl; 
+cout << "Byron Prather" << endl;
+
 =======
 >>>>>>> 0b68d2696bf827ee3ec0de1c5d26795890a9b6d4
 }
